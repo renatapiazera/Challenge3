b12b37fe9c5f996b85500a4afe2d5897b5e97a3a
Renata Piazera <piazerarenata@gmail.com>
2024-02-29 00:57:44 -0300

    docs: comments in code - Product class

--- src/main/java/com/product/Product.java
+++ src/main/java/com/product/Product.java
@@ -1,19 +1,20 @@
 package main.java.com.product;
-
+// A classe representa um produto com suas características básicas.
 public class Product {
 
-    private int id;
+    final private int id;  // Identificador único do produto.
     private String name;
     private String description;
     private double value;
-
+    // Construtor que permite a criação de um produto com atributos específicos.
     public Product(int id, String name, String description, double value) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.value = value;
     }
-
+    // Métodos de acesso para obter informações sobre o produto.
+    // Retorna o identificador único do produto.
     public int getId() {
         return id;
     }
@@ -21,20 +22,20 @@ public class Product {
     public String getName() {
         return name;
     }
-
+    // Define um novo nome para o produto.
     public void setName(String name) {
         this.name = name;
     }
-
+    // Define uma nova descrição para o produto.
     public void setDescription(String description) {
         this.description = description;
     }
-
-
+    // Define um novo valor monetário para o produto.
     public void setValue(double value) {
         this.value = value;
     }
-
+    // Retorna uma representação em formato de string do objeto Product.
+    //Formato JSON, conforme solicitado na Regra de Negócio.
     @Override
     public String toString() {
         return "{"
